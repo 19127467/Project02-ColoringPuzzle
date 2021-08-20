fb5d01076db88c4bd0ae93fc62dcc272b3faf2cb
NguyenThanhLuan27032001 <64632038+NguyenThanhLuan27032001@users.noreply.github.com>
2021-08-20 20:40:45 +0700

    update

--- CNF.py
+++ CNF.py
@@ -4,6 +4,8 @@ from tkinter import filedialog
 import os.path as path
 from itertools import combinations
 import tkinter as tkt
+from time import perf_counter
+from pysat.solvers import Glucose3
 
 # function to read the input file:
 # def readfile(file_name_in: str) -> Tuple[int, int, List[int]]:
@@ -186,6 +188,79 @@ def BFAssignment(row, col, m, n, result, input):
     return False
 
 
+#testing 
+#Function for solving input by using Pysat library:
+def pysatSolving(m: int, n: int, mat: List[int]) -> Tuple[List[int], float]:
+    clauses, result = [], []
+
+    time_start = perf_counter()
+    clauses = completeClauses(mat, m, n)
+
+    g = Glucose3()
+    for it in clauses:
+        g.add_clause([int(k) for k in it])
+    print(g.solve())
+    model = g.get_model()
+    print(model, '\n')
+
+    for i in range(m):
+        temp = []
+        for j in range(n):
+            if (i*n+j+1 in model):
+                temp.append(1)
+            else:
+                temp.append(0)
+        result.append(temp)
+
+    time_stop = perf_counter()
+
+    return result, time_stop - time_start
+
+
+#Function for solving input by using the Backtracking algorithm:
+def Backtracking_ver(m: int, n: int, mat: List[int]) -> Tuple[List[int], float]:
+    start = perf_counter()
+    #prepare needed variables:
+    result = []
+    cell_with_unnega_value = getPosCells(mat, m, n)
+
+    for _ in range(m):
+        temp = []
+        for _ in range(n):
+            temp.append(0)
+        result.append(temp)
+
+    #Backtracking:
+    if (not solveBTCells(0, cell_with_unnega_value, m, n, result, mat)):
+        for row in range(m):
+            for col in range(n):
+                result[row][col] = -1
+
+    end = perf_counter()
+    
+    return result, end - start
+
+
+#Function for solving input by using the Brute Force algorithm:
+def Brute_Force_ver(m: int, n: int, mat: List[int]) -> Tuple[List[int], float]:
+    start = perf_counter()
+    #prepare needed variables:
+    result = []
+    for _ in range(m):
+        temp = []
+        for _ in range(n):
+            temp.append(-1)
+        result.append(temp)
+
+    #Backtracking:
+    if (not BFAssignment (0, 0, m, n, result, mat)):
+        for row in range(m):
+            for col in range(n):
+                result[row][col] = -1
+
+    end = perf_counter()
+    
+    return result, end - start
 
 
 
--- main.py
+++ main.py
@@ -1,91 +1,4 @@
 from CNF import *
-from time import perf_counter
-from pysat.solvers import Glucose3
-
-#testing 
-#Function for solving input by using Pysat library:
-def Pysat_ver(m: int, n: int, mat: List[int]) -> Tuple[List[int], float]:
-    clauses, result = [], []
-
-    time_start = perf_counter()
-    clauses = completeClauses(mat, m, n)
-
-    g = Glucose3()
-    for it in clauses:
-        g.add_clause([int(k) for k in it])
-    print(g.solve())
-    model = g.get_model()
-    print(model, '\n')
-
-    """ printMatrix(mat)
-    print('')
-    for i in range(m):
-        for j in range(n):
-            if i*n+j+1 in model:
-                print('G', end=' ')
-            else:
-                print('_', end=' ')
-        print('') """
-    
-    for i in range(m):
-        temp = []
-        for j in range(n):
-            if (i*n+j+1 in model):
-                temp.append(1)
-            else:
-                temp.append(0)
-        result.append(temp)
-
-    time_stop = perf_counter()
-
-
-    return result, time_stop - time_start
-
-
-#Function for solving input by using the Backtracking algorithm:
-def Backtracking_ver(m: int, n: int, mat: List[int]) -> Tuple[List[int], float]:
-    start = perf_counter()
-    #prepare needed variables:
-    result = []
-    posCell = getPosCells(mat, m, n)
-
-    for _ in range(m):
-        temp = []
-        for _ in range(n):
-            temp.append(0)
-        result.append(temp)
-
-    #Backtracking:
-    if (not solveBTCells(0, posCell, m, n, result, mat)):
-        for row in range(m):
-            for col in range(n):
-                result[row][col] = -1
-
-    end = perf_counter()
-    
-    return result, end - start
-
-
-#Function for solving input by using the Brute Force algorithm:
-def Brute_Force_ver(m: int, n: int, mat: List[int]) -> Tuple[List[int], float]:
-    start = perf_counter()
-    #prepare needed variables:
-    result = []
-    for _ in range(m):
-        temp = []
-        for _ in range(n):
-            temp.append(-1)
-        result.append(temp)
-
-    #Backtracking:
-    if (not BFAssignment (0, 0, m, n, result, mat)):
-        for row in range(m):
-            for col in range(n):
-                result[row][col] = -1
-
-    end = perf_counter()
-    
-    return result, end - start
 
 
 def main():
@@ -93,7 +6,7 @@ def main():
     m, n, mat = readfile("input.txt")
 
     # pysat:
-    result_pysat, runtime_pysat = Pysat_ver(m, n, mat)
+    result_pysat, runtime_pysat = pysatSolving(m, n, mat)
     print("PySAT matrix:")
     printMatrix(result_pysat)
     print("Amount of time the algorithm spent:", runtime_pysat, 'second(s).')
